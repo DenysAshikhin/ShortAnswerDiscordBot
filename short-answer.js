1763a98878a92bc773110d4283e89eb0f98f3151
Denys Ashikhin <denysashikhin@gmail.com>
2020-04-20 16:54:02 -0400

    Tweaked fuzzy search parameter

--- short-answer.js
+++ short-answer.js
@@ -412,7 +412,7 @@ async function study(message, searches) {
         useExtendedSearch: false,
         minMatchCharLength: 3,
         shouldSort: true,
-        threshold: 0.6,
+        threshold: 0.8,
         location: 0,
         distance: 100,
         keys: [
