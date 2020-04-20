371452d0336d85d1299ab9e19f3cb1137c50042c
Denys Ashikhin <denysashikhin@gmail.com>
2020-04-20 16:54:31 -0400

    Tweaked again

--- short-answer.js
+++ short-answer.js
@@ -412,7 +412,7 @@ async function study(message, searches) {
         useExtendedSearch: false,
         minMatchCharLength: 3,
         shouldSort: true,
-        threshold: 0.8,
+        threshold: 0.4,
         location: 0,
         distance: 100,
         keys: [
